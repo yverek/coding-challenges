a84872bd852af3188ebbb66236942fa69ff7614b
Mauro Dattilo <mau.dattilo@gmail.com>
2020-11-04 13:25:44 +0100

    Fix Project Euler problem #0001 documentation

--- project-euler/[0001] - Multiples of 3 and 5/0001.js
+++ project-euler/[0001] - Multiples of 3 and 5/0001.js
@@ -1,7 +1,7 @@
 /*
- * @name:    Multiples of 3 and 5
- * @id:      0001
- * @url:     https://projecteuler.net/problem=1
+ * @name: Multiples of 3 and 5
+ * @id:   0001
+ * @url:  https://projecteuler.net/problem=1
  *
  * @problem:
  *   If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
