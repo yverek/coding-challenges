840f3e5bf4d21acac99d589fbba2b27a371d43ca
Mauro Dattilo <mau.dattilo@gmail.com>
2020-11-14 17:47:21 +0100

    Add test to problem #0001

--- project-euler/[0001] - Multiples of 3 and 5/0001.js
+++ project-euler/[0001] - Multiples of 3 and 5/0001.js
@@ -11,10 +11,12 @@
  * @solution: 233168
  */
 
-let sum = 0;
+export default function problem0001() {
+    let sum = 0;
 
-for (let i=0; i<1000; i++)
-    if (i % 3 === 0 || i % 5 === 0)
-        sum += i;
+    for (let i=0; i<1000; i++)
+        if (i % 3 === 0 || i % 5 === 0)
+            sum += i;
 
-console.log(`The solution is: ${sum}`);
+    return sum;
+}
---
+++ project-euler/[0001] - Multiples of 3 and 5/0001.test.js
@@ -0,0 +1,6 @@
+import { test } from "@jest/globals";
+import problem0001 from './0001';
+
+test('problem0001() should return 233168', () => {
+    expect(problem0001()).toBe(233168);
+});
