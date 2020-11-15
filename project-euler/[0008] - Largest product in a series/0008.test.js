568bc03fadfa5a458bda448db53633fbb56c2aac
Mauro Dattilo <mau.dattilo@gmail.com>
2020-11-15 15:19:45 +0100

    Add test to problem #0008

---
+++ project-euler/[0008] - Largest product in a series/0008.test.js
@@ -0,0 +1,6 @@
+import { test } from "@jest/globals";
+import problem0008 from './0008.js';
+
+test('problem0008() should return 23514624000', () => {
+    expect(problem0008()).toBe(23514624000);
+});
