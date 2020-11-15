79702d4ab00987b83add81fce1c88427dfe33dda
Mauro Dattilo <mau.dattilo@gmail.com>
2020-11-15 18:33:00 +0100

    Add test to problem #0013

---
+++ project-euler/[0013] - Large sum/0013.test.js
@@ -0,0 +1,6 @@
+import { test } from "@jest/globals";
+import problem0013 from './0013.js';
+
+test('problem0013() should return 5537376230', () => {
+    expect(problem0013()).toBe(5537376230);
+});
